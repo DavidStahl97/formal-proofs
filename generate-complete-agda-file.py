670cd5093d628da61126c3bcfe555ec38eea19de
David Stahl <david.stahl97@outlook.com>
2022-01-15 18:44:43 +0100

    fix linux path

--- generate-complete-agda-file.py
+++ generate-complete-agda-file.py
@@ -7,24 +7,32 @@ imports = []
 
 for file in Path(currentDirectory).rglob('*.agda'):
     relativeFilePath = os.path.relpath(str(file), currentDirectory)    
-    split = relativeFilePath.split('\\')
+    if relativeFilePath == 'everything.agda':
+        continue
+
+    split = os.path.split(relativeFilePath)
     assert len(split) > 0, split
 
-    fileName = split[len(split) - 1]
+    if split[0] == '':
+        split = split[1:]
+
+    fileName = split[-1]
     assert fileName.endswith('.agda'), fileName
         
     module = fileName.split('.agda')
     assert len(module) == 2, module
     assert module[1] == '', module
 
-    split[len(split) - 1] = module[0]
+    split = split[:-1] + (module[0],)
     completeModuleName = '.'.join(split)
     
     imports.append('import ' + completeModuleName + '\n')
 
-print('generated complete agda file:')
+print('generated complete agda file: \n\n')
 print(''.join(imports))
 
 everythingFile = open('everything.agda', 'w')
 everythingFile.write(''.join(imports))
 everythingFile.close()    
+
+# %%
